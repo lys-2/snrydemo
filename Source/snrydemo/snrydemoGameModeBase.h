5f62b60d4689bb1d040148556745a16e4c1cf4d9
!312 <nine@noeight.net>
2020-09-25 17:24:44 +0300

    up

---
+++ Source/snrydemo/snrydemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "snrydemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNRYDEMO_API AsnrydemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
