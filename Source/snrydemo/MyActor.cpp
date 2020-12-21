893071f87be4a3a7a9d66171f033fe8fa0446297
!312 <nine@noeight.net>
2020-12-21 05:34:44 +0300

    up

--- Source/snrydemo/MyActor.cpp
+++ Source/snrydemo/MyActor.cpp
@@ -1,15 +1,21 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MyActor.h"
-
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 AMyActor::AMyActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	TotalDamage = 200;
+
+}
+
+void AMyActor::MyFunction1(FString SomeString)
+{
+	//UE_LOG(LogTemp, Log, TEXT("123123 %s"), *SomeString);
 }
 
 // Called when the game starts or when spawned
@@ -17,8 +23,6 @@ void AMyActor::BeginPlay()
 {
 	Super::BeginPlay();
 
-
-	
 }
 
 // Called every frame
--- Source/snrydemo/MyActor.h
+++ Source/snrydemo/MyActor.h
@@ -10,10 +10,20 @@ UCLASS()
 class SNRYDEMO_API AMyActor : public AActor
 {
 	GENERATED_BODY()
-	
+
+	UFUNCTION(BlueprintCallable, Category = "123123")
+	static void MyFunction1(FString SomeString);
+
 public:	
 	// Sets default values for this actor's properties
 	AMyActor();
+	UPROPERTY(EditAnywhere, Category = "Damage")
+	int32 TotalDamage;
+
+	int32 TotalDamage2;
+	int threedim[111][111][111];
+
+
 
 protected:
 	// Called when the game starts or when spawned
