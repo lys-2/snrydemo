631f5a5bc7815bacc714ade472cf4a8b999e1c6c
!312 <nine@noeight.net>
2021-03-05 09:10:51 +0300

    up

--- andr/app/src/main/java/com/example/snry_remote/MainActivity.kt
+++ andr/app/src/main/java/com/example/snry_remote/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.snry_remote
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,15 +19,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        Log.d("TAG", "message")
+
+        val m = "hello from ANDROID123!"
+        val s = DatagramSocket()
+        val p = DatagramPacket(m.toByteArray(), m.length,
+                InetAddress.getByName("192.168.1.103"), 8900);
+
+        val t = Thread(Runnable {
+            s.send(p)
+        })
+
+
+
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            t.start()
         }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
         return true
     }
 
@@ -30,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
